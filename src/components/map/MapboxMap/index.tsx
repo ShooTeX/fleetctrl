672cc9067e6243cdfe86b933d117e73ac14bca7d
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2022-11-06 02:26:43 +0100

    add initmap function

--- src/components/map/MapboxMap/index.tsx
+++ src/components/map/MapboxMap/index.tsx
@@ -1,6 +1,7 @@
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useEffect, useRef, useState } from "react";
+import { initMap } from "../../../utils/map/init-map";
 
 mapboxgl.accessToken = "";
 export const MapboxMap = () => {
@@ -13,10 +14,14 @@ export const MapboxMap = () => {
         container: mapNode.current,
         accessToken: process.env.NEXT_PUBLIC_MAPBOX_TOKEN,
         style: "mapbox://styles/mapbox/dark-v10",
-        center: [9.993_682, 53.551_086],
-        zoom: 9,
+        // center: [9.993_682, 53.551_086],
+        // zoom: 9,
+        bounds: [
+          [9.729_561, 53.762_799],
+          [10.338_187, 53.383_792],
+        ],
       });
-
+      initMap(mapboxMap);
       setMap(mapboxMap);
     }
 
