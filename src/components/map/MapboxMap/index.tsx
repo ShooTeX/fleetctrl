2723e1a9dcb583d2f25a5fb258eb4ff3f64ec6c7
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2022-11-06 17:25:53 +0100

    mapprovider

--- src/components/map/MapboxMap/index.tsx
+++ src/components/map/MapboxMap/index.tsx
@@ -5,6 +5,7 @@ import { hhFeature } from "../../../utils/map/hh-feature";
 export const MapboxMap = () => {
   return (
     <Map
+      id="mainMap"
       initialViewState={{
         bounds: [
           [9.729_561, 53.762_799],
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import { MapProvider } from "react-map-gl";
 import { MapboxMap } from "../components/map/MapboxMap";
 
 // import { trpc } from "../utils/trpc";
@@ -15,7 +16,9 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="h-screen w-screen">
-        <MapboxMap />
+        <MapProvider>
+          <MapboxMap />
+        </MapProvider>
       </div>
     </>
   );
