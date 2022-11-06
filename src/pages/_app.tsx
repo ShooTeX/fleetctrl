e9cd7873dd78284ccb1e9f4464888f02a5a962b9
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2022-11-06 19:51:24 +0100

    change font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,16 @@
 import { type AppType } from "next/app";
-import { Inter_Tight } from "@next/font/google";
+import { Inter } from "@next/font/google";
+import { MapProvider } from "react-map-gl";
 
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
-import { MapProvider } from "react-map-gl";
 
-const openSans = Inter_Tight({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <main className={openSans.className}>
+    <main className={inter.className}>
       <MapProvider>
         <Component {...pageProps} />
       </MapProvider>
