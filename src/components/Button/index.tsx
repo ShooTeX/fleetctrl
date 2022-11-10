9a28c6389d8aeac7f0fbbf74c5e9986a1f302027
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2022-11-11 00:19:41 +0100

    use clsx

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,4 @@
+import { clsx } from "clsx";
 import type { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
 import type { IconType } from "react-icons";
 
@@ -13,7 +14,12 @@ export const Button = ({ children, Icon, ...rest }: ButtonProperties) => {
   return (
     <button
       type="button"
-      className="flex items-center space-x-2 rounded-lg bg-zinc-800 px-5 py-1 font-medium uppercase text-slate-50 transition-all ease-in-out hover:shadow-lg"
+      className={clsx(
+        "flex items-center space-x-2",
+        "rounded-lg bg-zinc-800 px-5 py-1",
+        "font-medium uppercase text-slate-50",
+        "transition-all ease-in-out hover:shadow-lg"
+      )}
       {...rest}
     >
       {!!Icon && <Icon className="text-xl text-zinc-400" />}
