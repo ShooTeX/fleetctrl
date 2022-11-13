213ff69c39092f4c43b7e05f24b04837e78601f2
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2022-11-13 20:17:09 +0100

    move markers into own component

--- src/components/map/Popup/index.tsx
+++ src/components/map/Popup/index.tsx
@@ -1,7 +1,21 @@
+import { IoMdArrowRoundDown } from "react-icons/io";
+import type { LngLat } from "react-map-gl";
+import { Marker } from "react-map-gl";
 import { ContextMenu } from "../../ContextMenu";
 
-export const Popup = () => (
-  <div className="ml-5 -mt-6">
-    <ContextMenu />
-  </div>
+interface PopupProperties {
+  position: LngLat;
+}
+
+export const Popup = ({ position }: PopupProperties) => (
+  <>
+    <Marker longitude={position.lng} latitude={position.lat} anchor="bottom">
+      <IoMdArrowRoundDown className="animate-bounce text-3xl leading-none text-white" />
+    </Marker>
+    <Marker longitude={position.lng} latitude={position.lat} anchor="top-left">
+      <div className="ml-5 -mt-6">
+        <ContextMenu />
+      </div>
+    </Marker>
+  </>
 );
