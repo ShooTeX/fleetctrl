da8d99ea73c115009531b4d1f9fe9d7ad6621daa
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2022-11-14 03:12:01 +0100

    more reusable popup

--- src/components/map/Popup/index.tsx
+++ src/components/map/Popup/index.tsx
@@ -1,23 +1,22 @@
 import { IoMdArrowRoundDown } from "react-icons/io";
 import type { LngLat } from "react-map-gl";
 import { Marker } from "react-map-gl";
-import { FaUserPlus } from "react-icons/fa";
+import type { ButtonGroupProperties } from "../../ButtonGroup";
 import { ButtonGroup } from "../../ButtonGroup";
 
-interface PopupProperties {
+type PopupProperties = {
+  buttons: ButtonGroupProperties["buttons"];
   position: LngLat;
-}
+};
 
-export const Popup = ({ position }: PopupProperties) => (
+export const Popup = ({ position, buttons }: PopupProperties) => (
   <>
     <Marker longitude={position.lng} latitude={position.lat} anchor="bottom">
       <IoMdArrowRoundDown className="animate-bounce text-3xl leading-none text-white" />
     </Marker>
     <Marker longitude={position.lng} latitude={position.lat} anchor="top-left">
       <div className="ml-5 -mt-6">
-        <ButtonGroup
-          buttons={[{ title: "hello", onClick: console.log, Icon: FaUserPlus }]}
-        />
+        <ButtonGroup buttons={buttons} />
       </div>
     </Marker>
   </>
