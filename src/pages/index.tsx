d4fa5f0d0ce1668b67f3c6bc5a13de7be96eb739
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2022-11-14 04:34:58 +0100

    add snackbar/actionbar area

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import { MdOutlineAddLocation } from "react-icons/md";
 import { Header } from "../components/Header";
 import { MapboxMap } from "../components/map/MapboxMap";
 import { MapControls } from "../components/map/MapControls";
@@ -22,6 +23,17 @@ const Home: NextPage = () => {
         <div className="flex h-full w-full bg-zinc-800">
           <NavSideBar />
           <div className="relative flex-1 overflow-hidden rounded-tl-xl">
+            <div className="absolute inset-x-0 top-4 z-50 m-auto flex justify-center">
+              <div className="flex divide-x-2 rounded-md bg-emerald-600 py-3 text-white drop-shadow-lg">
+                <div className="flex items-center space-x-2 px-5">
+                  <MdOutlineAddLocation className="text-lg text-emerald-100" />
+                  <span>Set a destination</span>
+                </div>
+                <button type="button" className="px-3 font-bold">
+                  cancel
+                </button>
+              </div>
+            </div>
             <div className="absolute right-4 top-4 z-50">
               <MapControls />
             </div>
