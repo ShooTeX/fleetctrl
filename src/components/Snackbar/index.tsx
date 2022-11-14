3b119e40d9a943f1b5e8e2a781a453fd1fb0f1b6
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2022-11-14 15:02:28 +0100

    move snackbar into own component

---
+++ src/components/Snackbar/index.tsx
@@ -0,0 +1,42 @@
+import clsx from "clsx";
+import type { IconType } from "react-icons";
+
+type SnackbarProperties = {
+  Icon?: IconType;
+  className?: string;
+  children: string;
+  action?: {
+    title: string;
+    onClick: () => void;
+  };
+};
+
+export const Snackbar = ({
+  Icon,
+  className,
+  children,
+  action,
+}: SnackbarProperties) => {
+  return (
+    <div
+      className={clsx(
+        "flex divide-x-2 rounded-md bg-emerald-600 py-3 text-white drop-shadow-lg",
+        className
+      )}
+    >
+      <div className="flex items-center space-x-2 px-5">
+        {!!Icon && <Icon className="text-lg text-emerald-100" />}
+        <span>{children}</span>
+      </div>
+      {!!action && (
+        <button
+          type="button"
+          onClick={action.onClick}
+          className="px-3 font-bold"
+        >
+          {action.title}
+        </button>
+      )}
+    </div>
+  );
+};
